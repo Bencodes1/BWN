e8e5cbacc664135704d286acbc574480174b0958
Bencodes1 <256.ben@gmail.com>
2021-04-07 12:37:41 -0400

    blank folder for notes proj

---
+++ notes/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.db.models.fields import DateTimeField
+
+# Create your models here.
+
+class Note(models.Model):
+    date_created = models.DateTimeField(auto_now_add = True)
+    date_updated = models.DateTimeField(auto_now_add = True)
+    title = models.CharField(max_length=160, blank = True, default = 'untitled')
+    body = models.TextField()
+
+    class Meta:
+        ordering = ['date_updated']
+
